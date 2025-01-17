765b302f0a5b01c10be98f7cffaac0784910a2f1
TatsuyaVexthorn <walkernight1010@gmail.com>
2025-01-18 00:30:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
+window.addEventListener('load', function() {
+    const loader = document.getElementById('loader');
+    const content = document.getElementById('content');
+
+    loader.style.display = 'none';
+    content.style.display = 'block';
+  });
+
+
+
+
 document.getElementById("myButton").addEventListener("click", function () {
 
     window.location.href = "https://www.google.com";
   });
+
